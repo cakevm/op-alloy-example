f94a5402353f0fcb87f57566b954fa4d6df8e19e
cakevm <cakevm@proton.me>
2025-02-10 19:29:36 +0100

    Use type hints for Provider to use Optimism

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use alloy_provider::{Provider, ProviderBuilder};
-use alloy_rpc_types::BlockId;
-use op_alloy_network::primitives::BlockTransactionsKind;
+use alloy_provider::{Identity, Provider, ProviderBuilder};
+use alloy_rpc_types::{BlockId, BlockTransactionsKind};
 use op_alloy_network::Optimism;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     let rpc_url = "https://base.merkle.io".parse()?;
-    let provider = ProviderBuilder::new().network::<Optimism>().on_http(rpc_url);
+    let provider = ProviderBuilder::<Identity, Identity, Optimism>::default().on_http(rpc_url);
 
     let block = provider.get_block(BlockId::latest(), BlockTransactionsKind::Full).await?;
     println!("Block: {:?}", block);
